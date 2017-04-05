4f32192da2db3110c473e15040105b99a4151ceb
Aleksander Alekseev <afiskon@gmail.com>
2017-04-05 16:25:35 +0300

    Cleanup code added to gen.sh

--- src/gen.sh
+++ src/gen.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-#rm ../*.html || true
-#rm -r ../author || true
-#rm -r ../author || true
-#rm -r ../category || true
-#rm -r ../rss || true
-#rm -r ../theme || true
+rm ../*.html || true
+rm -r ../author || true
+rm -r ../author || true
+rm -r ../category || true
+rm -r ../rss || true
+rm -r ../theme || true
 
 pelican --relative-urls --ignore-cache \
   --theme-path template/pelican-simplegrey \
